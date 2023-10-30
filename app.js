0d1236da2b313f08f1c2836517228d49f3c8e4db
alaxkarr <137056880+alaxkarr@users.noreply.github.com>
2023-10-30 16:22:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
+const http = require('http');
 
-const app = express();
-const PORT = 3001;
-
-app.use(express.json());
-app.use(cors());
-
-app.use(express.static('public')); // to set the path for css
-
-app.get('/', async (req, res) => {
-  res.send('App is running');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!  This is Testin api @@@@ \n');
 });
 
-// Set the views directory to the folder where your EJS template is located
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); // Set EJS as the view engine
-
-// views
-app.get('/signin', async (req, res) => {
-  res.render('signin');
-});
-
-app.get('*', function (req, res) {
-  res.status(404).render('404');
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = 3001;
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
 });
